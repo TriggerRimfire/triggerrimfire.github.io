409ae2ef66e7c2a4165f1ba766c4cca66084305a
TriggerRimfire <poonikinsthemagicdog@gmail.com>
2017-01-26 18:50:52 +0100

    forgot source

---
+++ _harp/_js/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return(
+      <h1>Oooh. It's.. Working! Get the cake!</h1>
+    )
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"))
